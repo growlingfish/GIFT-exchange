f3a8c6017e1da344745fa8d8f83a0b99879ec47d
Ben Bedwell <github@growlingfish.com>
2018-02-28 20:28:28 +0000

    Scroll

--- src/pages/openobject/openobject.ts
+++ src/pages/openobject/openobject.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
+import { Component, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { NavController, NavParams, ViewController, AlertController, Content } from 'ionic-angular';
 
 @Component({
   selector: 'page-openobject',
@@ -7,10 +7,12 @@ import { NavController, NavParams, ViewController, AlertController } from 'ionic
 })
 export class OpenObjectPage {
 
+  @ViewChild(Content) content: Content;
   private object: any;
   private part: number;
+  public scrollFAB: boolean = false;
   
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public alertCtrl: AlertController, private changeDetector : ChangeDetectorRef) {
     this.object = navParams.get('object');
     this.part = navParams.get('part');
   }
@@ -34,6 +36,24 @@ export class OpenObjectPage {
       buttons: ['OK']
     });
     alert.present();
+  }
+
+  ionViewDidLoad () {
+    setTimeout(function() {
+      var diff = this.content.scrollHeight - this.content.contentHeight;
+      if (diff > 0) {
+        this.scrollFAB = true;
+        this.changeDetector.detectChanges();
+        this.content.ionScroll.subscribe((scroll) => {
+          if (scroll.scrollTop < 0.8 * diff) {
+            this.scrollFAB = true;
+          } else {
+            this.scrollFAB = false;
+          }
+          this.changeDetector.detectChanges();
+        });
+      }
+    }.bind(this), 1000);
   }
 
 }
--- src/pages/reviewobject/reviewobject.ts
+++ src/pages/reviewobject/reviewobject.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { Component, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { NavController, NavParams, ViewController, Content } from 'ionic-angular';
 
 @Component({
   selector: 'page-reviewobject',
@@ -7,10 +7,12 @@ import { NavController, NavParams, ViewController } from 'ionic-angular';
 })
 export class ReviewObjectPage {
 
+  @ViewChild(Content) content: Content;
   private object: any;
   private part: number;
+  public scrollFAB: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, private changeDetector : ChangeDetectorRef) {
     this.object = navParams.get('object');
     this.part = navParams.get('part');
   }
@@ -19,4 +21,21 @@ export class ReviewObjectPage {
     this.viewCtrl.dismiss();
   }
 
+  ionViewDidLoad () {
+    setTimeout(function() {
+      var diff = this.content.scrollHeight - this.content.contentHeight;
+      if (diff > 0) {
+        this.scrollFAB = true;
+        this.changeDetector.detectChanges();
+        this.content.ionScroll.subscribe((scroll) => {
+          if (scroll.scrollTop < 0.8 * diff) {
+            this.scrollFAB = true;
+          } else {
+            this.scrollFAB = false;
+          }
+          this.changeDetector.detectChanges();
+        });
+      }
+    }.bind(this), 1000);
+  }
 }
